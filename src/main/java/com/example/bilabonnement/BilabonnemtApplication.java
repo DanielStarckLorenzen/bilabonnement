d3f741d0b6e53b0cdc594998de8a5391044b2189
daniellorenzen <dani141d@stud.kea.dk>
2022-11-21 09:46:05 +0100

    first commit

---
+++ src/main/java/com/example/bilabonnement/BilabonnemtApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnemtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilabonnemtApplication.class, args);
+    }
+
+}
