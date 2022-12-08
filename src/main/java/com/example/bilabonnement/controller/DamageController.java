30c0db27191ac8ee8b604f9abd9c404c311b3103
AugustG01 <augu8204@stud.kea.dk>
2022-12-08 13:07:22 +0100

    created more controllers and folders for html pages

---
+++ src/main/java/com/example/bilabonnement/controller/DamageController.java
@@ -0,0 +1,8 @@
+package com.example.bilabonnement.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DamageController {
+}
---
+++ src/main/java/com/example/bilabonnement/controller/DataRegController.java
@@ -0,0 +1,7 @@
+package com.example.bilabonnement.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataRegController {
+}
